7d7e7a2b1aa5fc935429c371dc05fff147a3e197
zhida86 <zhida86@gmail.com>
2021-01-17 17:33:18 +0800

    代码优化，setState的函数用法，异步特性，prevState

--- src/TodoItem.js
+++ src/TodoItem.js
@@ -1,23 +1,19 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 
 class TodoItem extends Component {
-
   constructor(props) {
     super(props);
-    this.handleClick = this.handleClick.bind(this)
+    this.handleClick = this.handleClick.bind(this);
   }
 
   render() {
-    return (
-      <div 
-        onClick={this.handleClick}>
-          {this.props.content}
-        </div>
-    )
+    const { content } = this.props;
+    return <div onClick={this.handleClick}>{content}</div>;
   }
 
   handleClick() {
-    this.props.deleteItem(this.props.index);
+    const { deleteItem, index } = this.props;
+    deleteItem(index);
   }
 }
 
--- src/TodoList.js
+++ src/TodoList.js
@@ -1,15 +1,17 @@
-import React, { Component, Fragment } from 'react';
-import './style.css';
-import TodoItem from './TodoItem';
+import React, { Component, Fragment } from "react";
+import TodoItem from "./TodoItem";
+import "./style.css";
 
 class TodoList extends Component {
-
   constructor(props) {
     super(props);
     this.state = {
-      inputValue: '',
-      list: []
-    }
+      inputValue: "",
+      list: [],
+    };
+    this.handleInputChange = this.handleInputChange.bind(this);
+    this.handleBtnClick = this.handleBtnClick.bind(this);
+    this.handleItemDelete = this.handleItemDelete.bind(this);
   }
 
   render() {
@@ -17,56 +19,52 @@ class TodoList extends Component {
       <Fragment>
         <div>
           <label htmlFor="insertArea">输入内容</label>
-          {/* 下面是一个input框 */}
           <input
             id="insertArea"
             className="input"
             value={this.state.inputValue}
-            onChange={this.handleInputChange.bind(this)}
+            onChange={this.handleInputChange}
           />
-          <button onClick={this.handleBtnClick.bind(this)}>提交</button>
+          <button onClick={this.handleBtnClick}>提交</button>
         </div>
-        <ul>
-          {
-            this.state.list.map((item, index) => {
-              return (
-                <div>
-                  <TodoItem 
-                    content={item} 
-                    index={index} 
-                    deleteItem={this.handleItemDelete.bind(this)}
-                  />
-                </div>
-              )
-            })
-          }
-        </ul>
+        <ul>{this.getTodoItem()}</ul>
       </Fragment>
-    )
+    );
+  }
+
+  getTodoItem() {
+    return this.state.list.map((item, index) => {
+      return (
+        <TodoItem
+          key={index}
+          content={item}
+          index={index}
+          deleteItem={this.handleItemDelete}
+        />
+      );
+    });
   }
 
   handleInputChange(e) {
-    this.setState({
-      inputValue: e.target.value
-    })
+    const value = e.target.value;
+    this.setState(() => ({
+      inputValue: value,
+    }));
   }
 
   handleBtnClick() {
-    this.setState({
-      list: [...this.state.list, this.state.inputValue],
-      inputValue: ''
-    })
+    this.setState((prevState) => ({
+      list: [...prevState.list, prevState.inputValue],
+      inputValue: "",
+    }));
   }
 
   handleItemDelete(index) {
-    // immutable
-    // state 不允许我们做任何的改变
-    // 创建副本
-    const list = [...this.state.list];
-    list.splice(index, 1);
-    this.setState({
-      list: list
-    })
+    this.setState((prevState) => {
+      const list = [...prevState.list];
+      list.splice(index, 1);
+      return { list };
+    });
   }
 }
 

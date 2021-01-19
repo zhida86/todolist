cb7a1763b2bbf10d5e78067d9ab67074da30f534
zhida <zhida@gmail.com>
2021-01-19 15:18:16 +0800

    redux基础知识，actionTypes，actionCreators

--- src/TodoList.js
+++ src/TodoList.js
@@ -1,31 +1,62 @@
 import React, { Component } from 'react';
 import 'antd/dist/antd.css';
 import { Input, Button, List } from 'antd';
-
-const data = [
-  'aaaaaaa',
-  'bbbbbbb',
-  'cccccccc',
-  'ddddddddd'
-]
+import store from './store';
+import { getInputChangeAction, getAddItemAction, getDeleteItemAction } from './store/actionCreators';
 
 class TodoList extends Component {
+
+  constructor(props) {
+    super(props);
+    this.state = store.getState();
+    this.handleInputChange = this.handleInputChange.bind(this);
+    this.handleStoreChange = this.handleStoreChange.bind(this);
+    this.handleBtnClick = this.handleBtnClick.bind(this);
+    store.subscribe(this.handleStoreChange);
+  }
 
   render() {
     return (
       <div style={{marginTop: '10px', marginLeft: '10px'}}>
         <div>
-          <Input placeholder='todo info' style={{width: '300px', marginRight: '10px'}} />
-          <Button type='primary'>提交</Button>
+          <Input 
+            value={this.state.inputValue} 
+            placeholder='todo info' 
+            style={{width: '300px', marginRight: '10px'}} 
+            onChange={this.handleInputChange}
+          />
+          <Button
+            type='primary'
+            onClick={this.handleBtnClick}
+          >提交</Button>
         </div>
         <List 
           style={{marginTop: '10px', width: '300px'}}
           bordered
-          dataSource={data}
-          renderItem={item => (<List.Item>{item}</List.Item>)}
+          dataSource={this.state.list}
+          renderItem={(item, index) => (<List.Item onClick={this.handleItemDelete.bind(this, index)}>{item}</List.Item>)}
         />
       </div>
     )
+  }
+
+  handleInputChange(e) {
+    const action = getInputChangeAction(e.target.value);
+    store.dispatch(action);
+  }
+
+  handleStoreChange() {
+    this.setState(store.getState());
+  }
+
+  handleBtnClick() {
+    const action = getAddItemAction();
+    store.dispatch(action)
+  }
+
+  handleItemDelete(index) {
+    const action = getDeleteItemAction(index);
+    store.dispatch(action);
   }
 }
 
